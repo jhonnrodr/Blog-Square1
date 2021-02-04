c848122ebf5841b1157b764d547aedf774ae18bf
Jhonn Rodriguez <jhonn7rodriguez@gmail.com>
2021-02-03 22:55:24 -0400

    Add fields slug in posts and index for publication_date, role in users, Add Sqlite in Phpunit

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
 {
@@ -22,9 +23,11 @@ class PostFactory extends Factory
      */
     public function definition()
     {
+        $title = $this->faker->text;
         return [
             'title' => $this->faker->sentence(3),
-            'description' => $this->faker->text,
+            'description' => $title,
+            'slug'             => Str::slug($title, "-") .'-'. random_int(2, 1000),
             'user_id'     => User::factory()->create()->id,
             'publication_date' => now()
         ];
